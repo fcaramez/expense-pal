454f9b242b621c62ce41f3ea7c54562431c88bb5
fcaramez <francisco.caramez.duarte@gmail.com>
2023-11-19 18:08:27 +0000

    MAIN-T-10 | adds initial boilerplate with t3 app and prisma

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Expense Pal</title>
+        <meta name="description" content="Expense Pal" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
---
+++ src/server/api/trpc.ts
@@ -0,0 +1,35 @@
+import { initTRPC } from "@trpc/server";
+import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
+import superjson from "superjson";
+import { ZodError } from "zod";
+
+import { db } from "@/server/db";
+
+type CreateContextOptions = Record<string, never>;
+
+const createInnerTRPCContext = (_opts: CreateContextOptions) => {
+  return {
+    db,
+  };
+};
+
+export const createTRPCContext = (_opts: CreateNextContextOptions) => {
+  return createInnerTRPCContext({});
+};
+
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
+});
+
+export const createTRPCRouter = t.router;
+export const publicProcedure = t.procedure;
